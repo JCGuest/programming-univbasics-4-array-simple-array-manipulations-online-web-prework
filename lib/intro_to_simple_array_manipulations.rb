cb575c10f6371c13d4c45752fd3a5ca052156732
John Guest <jcguest90@gmail.com>
2019-07-24 02:10:57 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,12 @@
+def using _push(array, string)
+  array.push(string)
+end
+def using_unshift(array, string)
+  array.unshift(string)
+end
+def using_pop(array)
+  array.using_pop
+end
+def pop_with_args(array)
+  array.pop(2)
+end
